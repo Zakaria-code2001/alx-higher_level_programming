a75d540d7eda29b861364d169b2e9b597b305ed3
Zakaria-code2001 <zakariamohammadi91@gmail.com>
2023-08-07 12:25:04 -0700

    9. Easter Egg

---
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+str = "Python is an interpreted, interactive, object-oriented programming\
+ language that combines remarkable power with very clear syntax"
+rught_str= str[38:66]
+print(str.format(rught_str))
