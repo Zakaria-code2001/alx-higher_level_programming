976be6d148b9e3c93c57cc1ba82dc23f7bd2f9b3
Zakaria-code2001 <zakariamohammadi91@gmail.com>
2023-08-08 09:36:33 -0700

    idsidd

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3  
 for alphaLetters in range(ord('a'), ord('z')+1):
     if alphaLetters != ord("e") and alphaLetters != ord("q"):
-        print(f"{alphaLetters:c}", end="")
+        print("{:c}".format(alphaLetters), end="")
