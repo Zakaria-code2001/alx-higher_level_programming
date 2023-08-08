93ef507860b253e20c797daf105fe1bc4d7e93f1
Zakaria-code2001 <zakariamohammadi91@gmail.com>
2023-08-08 09:06:48 -0700

    hdvbvds

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3  
 for alphaLetters in range(ord('a'), ord('z')+1):
     if alphaLetters == 'e' or alphaLetters == 'q':
-       continue
-    print("{:c}".format(alphaLetters), end="")
+        continue
+    else:
+        print("{:c}".format(alphaLetters), end="")
