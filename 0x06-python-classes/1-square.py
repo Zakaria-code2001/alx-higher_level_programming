ff367ee16b4d89d623040ac42d86d682b95b3191
Zakaria-code2001 <zakariamohammadi91@gmail.com>
2023-08-29 22:22:27 +0200

    square1

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,4 +1,14 @@
 #!/usr/bin/python3
+"""square classe identfication"""
+
+
 class Square:
+    """Square Representation"""
+
     def __init__(self, size):
+        """Square initialisation
+
+        Args:
+            size (int): new square size
+        """
         self.__size = size
