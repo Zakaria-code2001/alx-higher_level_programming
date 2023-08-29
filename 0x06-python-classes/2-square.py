b7c2bfbddb1fd3919328c9bff6140bd394c03349
Zakaria-code2001 <zakariamohammadi91@gmail.com>
2023-08-29 22:27:03 +0200

    fs

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,6 +1,16 @@
 #!/usr/bin/python3
+"""Square identification"""
+
+
 class Square:
+    """Square representation"""
+
     def __init__(self, size=0):
+        """Square initialization
+
+        Args:
+            size (int): new square size.
+        """
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
         elif size < 0:
--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -1,10 +1,22 @@
 #!/usr/bin/python3
+"""Square identification"""
+
+
 class Square:
+    """Square representation"""
+
     def __init__(self, size=0):
+        """Square initialization
+
+        Args:
+            size (int): new square size.
+        """
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
         elif size < 0:
             raise ValueError("size must be >= 0")
         self.__size = size
+
     def area(self):
+        """Return area of the square."""
         return (self.__size * self.__size)
