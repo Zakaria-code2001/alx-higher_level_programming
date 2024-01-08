0437b96aed758450d2ff65a3ee5f7bcfaa515edb
Zakaria-code2001 <zakariamohammadi91@gmail.com>
2024-01-08 19:26:57 +0100

    fix

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,6 +1,5 @@
 #!/usr/bin/node
-const Myvar = ['C is fun','Python is cool','JavaScript is amazing'];
+const Myvar = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 for (let variable = 0; variable < Myvar.length; variable++) {
-    console.log(Myvar[variable]);
-    
+  console.log(Myvar[variable]);
 }
