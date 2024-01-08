cbb8b8adfa75c643fb4cc3c086247ac645f453e5
Zakaria-code2001 <zakariamohammadi91@gmail.com>
2024-01-08 19:02:50 +0100

    task 5

---
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+
+const argv = process.argv[2];
+const numArgv = Number(argv);
+
+if (Number.isInteger(numArgv)) {
+    console.log(`My number: ${numArgv}`);
+} else {
+    console.log('Not a number');
+}
