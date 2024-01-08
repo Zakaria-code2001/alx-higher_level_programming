4bb43796d0d283b637082ca00dabd37a986fb591
Zakaria-code2001 <zakariamohammadi91@gmail.com>
2024-01-08 20:37:09 +0100

    fact

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,17 +1,7 @@
 #!/usr/local/bin/node
 
-const num = Number(process.argv[2]);
-
 function factorial (n) {
-  if (n === 0 || n === 1) {
-    return 1;
-  } else {
-    return n * factorial(n - 1);
-  }
+  return n === 0 || isNaN(n) ? 1 : n * factorial(n - 1);
 }
 
-if (!isNaN(num) && Number.isInteger(num) && num >= 0) {
-  console.log(factorial(num));
-} else {
-  console.log('1');
-}
+console.log(factorial(Number(process.argv[2])));
