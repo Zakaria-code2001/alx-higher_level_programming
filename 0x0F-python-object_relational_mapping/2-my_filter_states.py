2599f487031e86cfddf3f95d79cfa3e107571954
Zakaria-code2001 <zakariamohammadi91@gmail.com>
2024-01-14 10:08:32 -0800

    ds

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """
-Write a script that takes in an argument and displays all values in the states table of hbtn_0e_0_usa where name matches the argument.
+Script that takes in an argument and displays all values in the states table of hbtn_0e_0_usa
+where name matches the argument.
 """
 
 import MySQLdb
@@ -12,18 +13,22 @@ if __name__ == "__main__":
     database = sys.argv[3]
     state_name_searched = sys.argv[4]
 
+    # Connect to MySQL server on localhost at port 3306
     db = MySQLdb.connect(host="localhost", port=3306, user=username, passwd=password, db=database)
     cursor = db.cursor()
 
-    state = state_name_searched  # Assign the state_name_searched directly to state
-    query = f"SELECT * FROM states WHERE name LIKE '{state}%' ORDER BY id ASC"
+    # Use format to create the SQL query with the user input
+    query = "SELECT * FROM states WHERE name = '{}' ORDER BY id ASC".format(state_name_searched)
     cursor.execute(query)
 
+    # Fetch all the rows using fetchall() method
     data = cursor.fetchall()
 
+    # Display results
     for row in data:
         print(row)
 
+    # Close the cursor and database connection
     cursor.close()
     db.close()
 
