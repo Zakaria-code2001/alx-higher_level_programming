3bc33afa176e8fcae16c888f201862eccd61fccd
Zakaria-code2001 <zakariamohammadi91@gmail.com>
2024-01-14 09:45:09 -0800

    fss

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -2,31 +2,38 @@
 """
 Script to retrieve and display all states from the 'states' table in a MySQL database
 """
+
 import MySQLdb
 import sys
 
 if __name__ == '__main__':
-    mysql_username = sys.argv[1]
-    mysql_password = sys.argv[2]
-    database_name = sys.argv[3]
+    try:
+        mysql_username = sys.argv[1]
+        mysql_password = sys.argv[2]
+        database_name = sys.argv[3]
 
-    db = MySQLdb.connect(
-        host='localhost',
-        user=mysql_username,
-        passwd=mysql_password,
-        db=database_name,
-        port=3306
-    )
+        db = MySQLdb.connect(
+            host='localhost',
+            user=mysql_username,
+            passwd=mysql_password,
+            db=database_name,
+            port=3306
+        )
 
-    cursor = db.cursor()
+        cursor = db.cursor()
 
-    query = "SELECT * FROM states ORDER BY id ASC;"
-    cursor.execute(query)
-    results = cursor.fetchall()
+        query = "SELECT * FROM states ORDER BY id ASC;"
+        cursor.execute(query)
+        results = cursor.fetchall()
 
-    for x in results:
-        print(x)
-      
-    cursor.close()
-    db.close()
+        for x in results:
+            print(x)
+
+    except MySQLdb.Error as e:
+        print(f"Error: {e}")
+
+    finally:
+        if 'db' in locals() and db:
+            cursor.close()
+            db.close()
 
