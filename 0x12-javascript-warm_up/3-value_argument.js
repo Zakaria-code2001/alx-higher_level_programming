bcc3005f803b3c3de2d650f559c8fa5fb9f1e628
Zakaria-code2001 <zakariamohammadi91@gmail.com>
2024-03-11 21:38:53 +0100

    task 3

---
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -0,0 +1,12 @@
+#!/usr/local/bin/node
+
+const inputs = process.argv;
+
+const argsCount = process.argv.length - 2;
+const arg1 = inputs[2];
+
+if (argsCount === 0) {
+  console.log('No argument');
+} else if (argsCount === 1) {
+  console.log(arg1);
+}
