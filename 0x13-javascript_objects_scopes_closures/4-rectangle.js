fea3f6936e8d85bacf8a72c0e649415a98adf5e0
Zakaria-code2001 <zakariamohammadi91@gmail.com>
2024-03-12 17:24:15 +0100

    task 4

---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,29 @@
+#!/usr/bin/node
+
+class Rectangle {
+  constructor (w, h) {
+    if (w >= 0 && h >= 0 && Number.isInteger(w) && Number.isInteger(h)) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    for (let index = 0; index < this.height; index++) {
+      console.log('X'.repeat(this.width));
+    }
+  }
+
+  double () {
+    this.width *= 2;
+    this.height *= 2;
+  }
+
+  rotate () {
+    const temp = this.width;
+    this.width = this.height;
+    this.height = temp;
+  }
+}
+
+module.exports = Rectangle;
