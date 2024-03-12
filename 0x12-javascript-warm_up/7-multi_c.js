5169f95662e177f6c652303465c8d7461d63c9ae
Zakaria-code2001 <zakariamohammadi91@gmail.com>
2024-03-12 10:21:58 +0100

    task 7

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -3,12 +3,12 @@
 const c = 'C is fun';
 const inputs = process.argv;
 
-const n_of_times = inputs[2];
+const nOfTimes = inputs[2];
 
-if (isNaN(n_of_times) || n_of_times === undefined) {
+if (isNaN(nOfTimes) || nOfTimes === undefined) {
   console.log('Missing number of occurrences');
 } else {
-  for (let index = 0; index < n_of_times; index++) {
+  for (let index = 0; index < nOfTimes; index++) {
     console.log(c);
   }
 }
