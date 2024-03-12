458ca3748d2b24e02f3cc4a1d80ed2b6e89bb998
Zakaria-code2001 <zakariamohammadi91@gmail.com>
2024-03-12 17:12:59 +0100

    task 2

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -5,9 +5,7 @@ class Rectangle {
     if (w >= 0 && h >= 0 && Number.isInteger(w) && Number.isInteger(h)) {
       this.width = w;
       this.height = h;
-    } else {
-        let Obj = new Obj() 
-    }
+    } 
   }
 }
 
