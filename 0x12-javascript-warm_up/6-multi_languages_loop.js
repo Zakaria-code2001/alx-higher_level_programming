80c771be8a12e5d95745254c96a727316190e91d
Zakaria-code2001 <zakariamohammadi91@gmail.com>
2024-03-12 10:05:24 +0100

    task 6

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,8 +1,7 @@
 #!/usr/bin/node
 
-const languages = ["C is fun", "Python is cool", "JavaScript is amazing"];
+const languages = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 for (let index = 0; index < languages.length; index++) {
-    const element = languages[index];
-    console.log(element)
-    
+  const element = languages[index];
+  console.log(element);
 }
