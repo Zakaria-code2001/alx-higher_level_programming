e21f636bc5ec7fa2b6102fec169674d5ede27371
Zakaria-code2001 <zakariamohammadi91@gmail.com>
2024-03-12 11:07:36 +0100

    task 11

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,13 +1,13 @@
-#!/usr/bin/node
+#!/usr/local/bin/node
 
 const nums = [];
 
 for (let i = 2; i < process.argv.length; i++) {
-  nums.push(process.argv[i]);
+  nums.push(parseInt(process.argv[i]));
 }
 
 nums.sort();
 const secondBigger = nums[nums.length - 2];
-if (isNaN(secondBigger) || secondBigger === undefined || nums.length <2) {
+if (isNaN(secondBigger) || secondBigger === undefined || nums.length < 2) {
   console.log(0);
 } else { console.log(secondBigger); }
