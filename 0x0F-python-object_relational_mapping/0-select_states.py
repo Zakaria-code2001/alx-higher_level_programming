1d73e12a40adae69a6aee0847aac272e0720c6a1
Zakaria-code2001 <zakariamohammadi91@gmail.com>
2024-03-15 11:48:06 +0100

    update

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,44 +1,28 @@
 #!/usr/bin/python3
-"""
-Script to query states from a MySQL database.
-"""
-
 import MySQLdb
 import sys
 
-def query_states(username, password, db_name):
-    """
-    Query states from the specified MySQL database.
 
-    Args:
-        username (str): The username to connect to the MySQL database.
-        password (str): The password to connect to the MySQL database.
-        db_name (str): The name of the MySQL database.
+if __name__ == "__main__":
+    username = sys.argv[1]
+    password = sys.argv[2]
+    db_name = sys.argv[3]
 
-    Returns:
-        list: A list of tuples representing the queried states.
-    """
-    db = MySQLdb.connect(host="localhost", port=3306, user=username, passwd=password, db=db_name)
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=username,
+        passwd=password,
+        db=db_name
+    )
     cursor = db.cursor()
 
     query = "SELECT * FROM states ORDER BY id ASC"
     cursor.execute(query)
     results = cursor.fetchall()
 
+    for row in results:
+        print(row)
+
     cursor.close()
     db.close()
-
-    return results
-
-if __name__ == "__main__":
-    if len(sys.argv) != 4:
-        print("Usage: {} <username> <password> <db_name>".format(sys.argv[0]))
-        sys.exit(1)
-
-    username = sys.argv[1]
-    password = sys.argv[2]
-    db_name = sys.argv[3]
-
-    states = query_states(username, password, db_name)
-    for state in states:
-        print(state)
