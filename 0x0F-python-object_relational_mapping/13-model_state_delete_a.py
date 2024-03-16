558da0e4a3682e53a33f6611990d235b163a9edf
Zakaria-code2001 <zakariamohammadi91@gmail.com>
2024-03-16 13:32:38 -0700

    sf

---
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+
+"""
+Connect to a MySQL server running on localhost at port 3306
+and lists all State objects from the database hbtn_0e_6_usa
+"""
+from model_state import Base, State
+import sys
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+if __name__ == "__main__":
+    username = sys.argv[1]
+    password = sys.argv[2]
+    db_name = sys.argv[3]
+
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+        username, password, db_name), pool_pre_ping=False)
+
+    Session = sessionmaker(bind=engine)
+
+    session = Session()
+    Base.metadata.create_all(engine)
+
+    stato = session.query(State).filter(State.name.like('%a%')).all()
+    for state in stato:
+        session.delete(state)
+    session.commit()
+    session.close()
