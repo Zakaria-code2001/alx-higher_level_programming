f8cd2a38123d3ac3219f5539061d8801b7542806
Zakaria-code2001 <zakariamohammadi91@gmail.com>
2024-03-16 13:31:33 +0100

    8

---
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+
+"""
+Connect to a MySQL server running on localhost at port 3306
+and lists all State objects from the database hbtn_0e_6_usa
+"""
+from model_state import Base, State
+import sys
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+if __name__ == "__main__":
+    username = sys.argv[1]
+    password = sys.argv[2]
+    db_name = sys.argv[3]
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+        username, password, db_name), pool_pre_ping=True)
+
+    Session = sessionmaker(bind=engine)
+
+    session = Session()
+    Base.metadata.create_all(engine)
+
+    stato = session.query(State).order_by(State.id)[1:2]
+    for state in stato:
+        print("{}: {}".format(state.id, state.name))
+    session.close()
