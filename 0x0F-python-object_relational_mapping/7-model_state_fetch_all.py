c8ac91575c845a15f6928559981387815ad2dc4f
Zakaria-code2001 <zakariamohammadi91@gmail.com>
2024-03-16 13:03:32 +0100

    test

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -4,19 +4,22 @@
 Connect to a MySQL server running on localhost at port 3306
 and lists all State objects from the database hbtn_0e_6_usa
 """
-from sqlalchemy.orm import sessionmaker
-from sqlalchemy import create_engine
 from model_state import Base, State
-import sys
+from sys import argv
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
 
-engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
-        sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+if __name__ == "__main__":
 
-Session = sessionmaker(bind=engine)
-session = Session()
-Base.metadata.create_all(engine)
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+        argv[1], argv[2], argv[3]), pool_pre_ping=True)
 
-stato = session.query(State).order_by(State.id).all()
-for state in stato:
-    print("{}: {}".format(state.id, state.name))
-session.close()
+    Session = sessionmaker(bind=engine)
+
+    session = Session()
+    Base.metadata.create_all(engine)
+
+    stato = session.query(State).order_by(State.id).all()
+    for state in stato:
+        print("{}: {}".format(state.id, state.name))
+    session.close()
