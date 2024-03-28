f09326ef184f96bba5ab4e9f6ad32d2bbb135a57
Zakaria-code2001 <zakariamohammadi91@gmail.com>
2024-03-28 14:30:11 -0700

    5. Response header value #1

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -5,10 +5,10 @@ import requests
 
 if __name__ == "__main__":
     url = 'https://alx-intranet.hbtn.io/status'
-    
+
     response = requests.get(url)
     html = response.text
-    
+
     print("Body response:")
     print("\t- type:", type(html))
     print("\t- content:", html)
