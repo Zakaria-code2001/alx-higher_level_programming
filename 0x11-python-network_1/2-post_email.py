74a903a829fb534fe7671e90d5523d35b0a33495
Zakaria-code2001 <zakariamohammadi91@gmail.com>
2024-03-28 14:19:48 -0700

    2. POST an email #0

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
-"""write a Python script that takes in a URL, sends a request to the URL and displays the value"""
+"""
+write a Python script that takes in a URL,
+sends a request to the URL and displays the value
+"""
 import urllib.request
 import urllib.parse
 import sys
@@ -7,12 +10,8 @@ import sys
 if __name__ == "__main__":
     url = sys.argv[1]
     email = sys.argv[2]
-
     data = urllib.parse.urlencode({'email': email}).encode()
-
-
     with urllib.request.urlopen(url, data=data) as response:
         html = response.read()
         decoded_html = html.decode('utf-8')
         print(decoded_html)
-
